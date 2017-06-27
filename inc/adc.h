588b91c68ecc78129ffa1a254f76431710fa4fa4
dangorogoro <dangorogoro0802@gmail.com>
2017-06-28 01:23:31 +0900

    Set sensor parameters and comment out

--- inc/adc.h
+++ inc/adc.h
@@ -1,5 +1,6 @@
 #ifndef MY_ADC_H
 #define MY_ADC_H
+/*
 extern uint16_t race_sensor0;
 extern uint16_t race_sensor1;
 extern uint16_t race_sensor2;
@@ -8,6 +9,7 @@ extern uint16_t line_color0;
 extern uint16_t line_color1;
 extern uint16_t line_color2;
 extern uint16_t line_color3;
+*/
 void ADC_setting();
 void battery_check();
 void start_ADC();
