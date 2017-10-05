a9f784c64072ef0d925796ba62ca339091cb080f
dangorogoro <dangorogoro0802@gmail.com>
2017-10-06 01:22:05 +0900

    Add USART mode for when I use Nucleo

--- src/my_usart.c
+++ src/my_usart.c
@@ -1,7 +1,8 @@
 #include "mine.h"
 void USART_setting(){
+	//RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART1,ENABLE);
 	//RCC_APB1PeriphClockCmd(RCC_APB1Periph_USART2,ENABLE);
-	RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART1,ENABLE);
+	RCC_APB1PeriphClockCmd(RCC_APB1Periph_USART3, ENABLE);
 	USART_InitTypeDef USART_InitStructure;
 	USART_InitStructure.USART_BaudRate=9600;
 	USART_InitStructure.USART_WordLength=USART_WordLength_8b;
@@ -9,8 +10,10 @@ void USART_setting(){
 	USART_InitStructure.USART_Parity=USART_Parity_No;
 	USART_InitStructure.USART_HardwareFlowControl=USART_HardwareFlowControl_None;
 	USART_InitStructure.USART_Mode=USART_Mode_Tx;
-	USART_Init(USART1,&USART_InitStructure);
-	USART_Cmd(USART1,ENABLE);
+	//USART_Init(USART1,&USART_InitStructure);
+	//USART_Cmd(USART1,ENABLE);
+	USART_Init(USART3,&USART_InitStructure);
+	USART_Cmd(USART3,ENABLE);
 }
 void USART_putc(USART_TypeDef* USARTx,char c){
 	while(!(USARTx->SR & 0x00000040)); 
@@ -26,7 +29,8 @@ void USART_printf(const char* format, ...){
 	char *str;
 	str = (char *)malloc(len+1);
 	vsprintf(str,format,list);
-	USART_puts(USART1,str);
+	USART_puts(USART3,str);
+	//USART_puts(USART2,str);
 	free(str);
 	va_end(list);
 	return ;
