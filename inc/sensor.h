c7053727adc815dc58b0c4e383882712e2d5989c
dangorogoro <dangorogoro0802@gmail.com>
2017-10-06 01:04:20 +0900

    Add sensor functions

--- inc/sensor.h
+++ inc/sensor.h
@@ -1,6 +1,7 @@
 #ifndef MY_SENSOR_H
 #define MY_SENSOR_H
 extern uint16_t led_1,led_2,led_3,led_4;
+extern uint16_t sensor_sub;
 void led_flash_setting();
 void led_flash();
 void led_get();
@@ -8,6 +9,7 @@ void led_stop();
 uint8_t read_wall(uint8_t IndexDirection);
 void sensor_works();
 void reset_led();
+void mouse_start();
 extern volatile uint8_t SENSOR_start;
 extern volatile uint8_t SENSOR_reset;
 #endif
