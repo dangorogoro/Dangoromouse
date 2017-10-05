5bf711e46be3d76a37e728a3f25cdf44db24b0f3
dangorogoro <dangorogoro0802@gmail.com>
2017-10-06 00:23:43 +0900

    Change writing style

--- src/param.cpp
+++ src/param.cpp
@@ -39,17 +39,17 @@ uint16_t Param::get_accel_param(){
 	return speed_param[3];
 }
 void ParamList::setting(){
-Param param1(0,100,100,10);
-Param param2(0,300,300,10);
-Param param3(0,600,600,10);
-Param param4(0,1200,600,10);
-Param param5(0,1000,1000,5);
-Param param6(0,1500,1000,10);
+Param param0(0,100,100,10);
+Param param1(0,300,300,10);
+Param param2(0,600,600,10);
+Param param3(0,1200,600,10);
+Param param4(0,1000,1000,5);
+Param param5(0,1500,1000,10);
 
+param_list.push_back(param0);
 param_list.push_back(param1);
 param_list.push_back(param2);
 param_list.push_back(param3);
 param_list.push_back(param4);
 param_list.push_back(param5);
-param_list.push_back(param6);
 }
