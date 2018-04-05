705f78283dc6d957b3f4d09f6e7d15a25e346407
dangorogoro <dangorogoro0802@gmail.com>
2018-04-05 10:50:52 +0900

    Add GPIO configuration for the suction fan

--- src/config.c
+++ src/config.c
@@ -100,6 +100,17 @@ void GPIO_setting(){
 	GPIO_Init(GPIOB,&GPIO_InitStructure);
 	GPIO_StructInit(&GPIO_InitStructure);
 
+	//Suction
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_8 | GPIO_Pin_9;
+	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_DOWN;
+	GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
+	GPIO_PinAFConfig(GPIOA,GPIO_PinSource8,GPIO_AF_TIM1);
+	GPIO_PinAFConfig(GPIOA,GPIO_PinSource9,GPIO_AF_TIM1);
+	GPIO_Init(GPIOA,&GPIO_InitStructure);
+	GPIO_StructInit(&GPIO_InitStructure);
+
 	//MPU6500
 	GPIO_InitStructure.GPIO_Pin=GPIO_Pin_5 | GPIO_Pin_6 | GPIO_Pin_7;
 	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
